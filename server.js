842f0203c1f5af62183221772aead95ee161952d
nikossyr <b_eliever@hotmail.com>
2020-09-26 19:01:56 +0300

    Installed the cors dependency to allow cross-origin requests to the server. Also implemented the cors in server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const colors = require('colors');
 const schema = require('./schema')
-
+const cors = require('cors');
 
 const app = express();
+
+// Allow cross-origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
