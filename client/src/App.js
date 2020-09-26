934a3fa13603bae3a70f7d60f6ab437461e8e513
nikossyr <b_eliever@hotmail.com>
2020-09-26 18:04:20 +0300

    Created a react app in client folder. Basic cleanup and spacex logo insertion as a page title. No functionality in frontend other than that.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+    return (
+        <div className="App">
+            <img src={logo} alt="SpaceX logo" style={{width: 300, display: "block", margin: "auto"}}/>
+        </div>
+    );
+}
+
+export default App;
